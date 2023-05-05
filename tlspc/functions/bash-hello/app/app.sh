c890c86c4e24dffb10c68316e2e26bd47b507c6c
Alan McGinlay <amcginlay@gmail.com>
2023-05-05 16:12:33 +0100

    explain why bash won't cut it, yet!

--- tlspc/functions/bash-hello/app/app.sh
+++ tlspc/functions/bash-hello/app/app.sh
@@ -3,4 +3,7 @@ function handler () {
 
     RESPONSE="{\"statusCode\": 200, \"body\": \"Hello from Lambda!\"}"
     echo $RESPONSE
+
+    # no further progress here until/unless we can port the cfnrespone module to bash:
+    # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-lambda-function-code-cfnresponsemodule.html
 }
