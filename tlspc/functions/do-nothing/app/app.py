f9b8ba72e7e9b3e008ddcd1a401fe37237ecbeb3
Alan McGinlay <amcginlay@gmail.com>
2023-05-06 21:33:20 +0000

    added CRUD handlers

--- tlspc/functions/do-nothing/app/app.py
+++ tlspc/functions/do-nothing/app/app.py
@@ -1,11 +1,49 @@
-import json
+import logging
+import cfnresponse
 
-# import requests
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def create_handler(event, context):
+    requestInfo = 'RequestType: Create'
+    logger.info(requestInfo)
+    # code here
+    responseData = {}
+    responseData['message'] = requestInfo
+    cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
+
+def update_handler(event, context):
+    requestInfo = 'RequestType: Update'
+    logger.info(requestInfo)
+    # code here
+    responseData = {}
+    responseData['message'] = requestInfo
+    cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
+
+def delete_handler(event, context):
+    requestInfo = 'RequestType: Delete'
+    logger.info(requestInfo)
+    # code here
+    responseData = {}
+    responseData['message'] = requestInfo
+    cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
+
+def default_handler(event, context):
+    requestInfo = 'RequestType: UNKNOWN'
+    logger.info(requestInfo)
+    # code here
+    responseData = {}
+    responseData['message'] = requestInfo
+    cfnresponse.send(event, context, cfnresponse.FAILED, responseData)
 
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "nothing done",
-        }),
+    logger.info('Received event: ' + str(event))
+
+    requestTypeHandlers = {
+        'Create': create_handler,
+        'Update': update_handler,
+        'Delete': delete_handler
     }
+    
+    requestTypeHandler = requestTypeHandlers.get(event.get('RequestType'), default_handler)
+    requestTypeHandler(event, context)
