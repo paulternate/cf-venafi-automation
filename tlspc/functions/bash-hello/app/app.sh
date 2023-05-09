55c994a8352e767e0418c9815caf53cd6c021522
Alan McGinlay <amcginlay@gmail.com>
2023-05-09 08:59:31 +0100

    bash lambda is not gonna fly this time. removing.

--- tlspc/functions/bash-hello/app/app.sh
+++
@@ -1,9 +0,0 @@
-function lambda_handler() {
-    EVENT_DATA=$1
-
-    RESPONSE="{\"statusCode\": 200, \"body\": \"Hello from Lambda!\"}"
-    echo $RESPONSE
-
-    # no further progress here until/unless we can port the cfnrespone module to bash:
-    # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-lambda-function-code-cfnresponsemodule.html
-}
