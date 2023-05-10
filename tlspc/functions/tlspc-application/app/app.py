148bf8bb33de16ad764dbe6ce964fbc688963503
Alan McGinlay <amcginlay@gmail.com>
2023-05-10 18:54:26 +0100

    Major refactoring to simplify completion of Create/Update/Delete functions. Standardised on AppGuid (capital A).

--- tlspc/functions/tlspc-application/app/app.py
+++ tlspc/functions/tlspc-application/app/app.py
@@ -8,26 +8,14 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 http = urllib3.PoolManager()
 
-def create_handler(event, context):
-    responseData = {}
-    requestInfo = 'RequestType: Create'
-    logger.info(requestInfo)
-    ###########
-    # code here
-    ###########
+def get_parameters(event):
     api_key=(str(event['ResourceProperties']['TLSPCAPIKey']))
     app_name=(str(event['ResourceProperties']['AppName']))
     issuing_template_name=(str(event['ResourceProperties']['IssuingTemplateName']))
-    cert_authority=(str(event['ResourceProperties']['CertificateAuthority']))    
-    logger.info("getting owner_id")
-    response = http.request(
-        'GET',
-        'https://api.venafi.cloud/v1/useraccounts',
-        headers={
-            'accept': 'application/json',
-            'tppl-api-key': api_key
-        })
-    owner_id = json.loads(response.data.decode('utf-8'))['user']['id']
+    cert_authority=(str(event['ResourceProperties']['CertificateAuthority']))
+    return api_key,app_name,issuing_template_name,cert_authority
+
+def get_template_id(api_key, issuing_template_name, cert_authority):
     logger.info("getting template_id")
     response = http.request(
         'GET',
@@ -41,6 +29,22 @@ def create_handler(event, context):
             if t['name'] == issuing_template_name and t['certificateAuthority'] == cert_authority),
         None
     )
+    
+    return template_id
+
+def get_current_user_id(api_key):
+    logger.info("getting owner_id")
+    response = http.request(
+        'GET',
+        'https://api.venafi.cloud/v1/useraccounts',
+        headers={
+            'accept': 'application/json',
+            'tppl-api-key': api_key
+        })
+    owner_id = json.loads(response.data.decode('utf-8'))['user']['id']
+    return owner_id
+
+def build_payload(app_name, issuing_template_name, owner_id, template_id):
     data = {
         "name": app_name,
         "ownerIdsAndTypes": [ 
@@ -54,6 +58,29 @@ def create_handler(event, context):
         }
     }
     logger.info("api payload created: data=" + str(data))
+    return data
+
+def get_app_guid(event):
+    stackId = event.get('StackId')
+    logger.info('stackId=' + stackId)
+    cfn_client = boto3.client('cloudformation')
+    stacks = cfn_client.describe_stacks(StackName = stackId)
+    logger.info('stacks=' + str(stacks))
+    app_guid = next((output['OutputValue'] for output in stacks['Stacks'][0]['Outputs'] if output['OutputKey'] == 'AppGUID'), None)
+    logger.info('app_guid=' + app_guid)
+    return app_guid
+
+def create_handler(event, context):
+    responseData = {}
+    requestInfo = 'RequestType: Create'
+    logger.info(requestInfo)
+    ###########
+    # code here
+    ###########
+    api_key, app_name, issuing_template_name, cert_authority = get_parameters(event)    
+    owner_id = get_current_user_id(api_key)
+    template_id = get_template_id(api_key, issuing_template_name, cert_authority)
+    payload = build_payload(app_name, issuing_template_name, owner_id, template_id)
     response = http.request(
         'POST',
         'https://api.venafi.cloud/outagedetection/v1/applications',
@@ -62,10 +89,10 @@ def create_handler(event, context):
             'content-type': 'application/json',
             'tppl-api-key': api_key
         },
-        body=json.dumps(data).encode('utf-8')
+        body=json.dumps(payload).encode('utf-8')
     )
-    logger.info("Create: response=" + str(response))
-    responseData['appGUID'] = json.loads(response.data.decode('utf-8'))['applications'][0]['id']
+    logger.info("response=" + str(response))
+    responseData['AppGUID'] = json.loads(response.data.decode('utf-8'))['applications'][0]['id']
     ###########
     responseData['message'] = requestInfo
     return responseData
@@ -77,7 +104,21 @@ def update_handler(event, context):
     ###########
     # code here
     ###########
-    # TBD needs a PUT request on https://api.venafi.cloud/outagedetection/v1/applications/<APP_GUID>
+    app_guid = get_app_guid(event)
+    api_key, app_name, issuing_template_name, cert_authority = get_parameters(event)   
+    owner_id = get_current_user_id(api_key)
+    template_id = get_template_id(api_key, issuing_template_name, cert_authority)
+    payload = build_payload(app_name, issuing_template_name, owner_id, template_id)
+    response = http.request(
+        'PUT',
+        'https://api.venafi.cloud/outagedetection/v1/applications/' + app_guid,
+        headers={
+            'accept': '*/*',
+            'tppl-api-key': api_key
+        },
+        body=json.dumps(payload).encode('utf-8')
+    )
+    logger.info('response=' + str(response))
     ###########
     responseData['message'] = requestInfo
     return responseData
@@ -89,17 +130,11 @@ def delete_handler(event, context):
     ###########
     # code here
     ###########
-    api_key=(str(event['ResourceProperties']['TLSPCAPIKey']))
-    stackId = event.get('StackId')
-    logger.info('stackId=' + stackId)
-    cfn_client = boto3.client('cloudformation')
-    stacks = cfn_client.describe_stacks(StackName = stackId)
-    logger.info('stacks=' + str(stacks))
-    appGUID = next((output['OutputValue'] for output in stacks['Stacks'][0]['Outputs'] if output['OutputKey'] == 'AppGUID'), None)
-    logger.info('appGUID=' + appGUID)
+    app_guid = get_app_guid(event)
+    api_key, _, _, _ = get_parameters(event)   
     response = http.request(
-        'DELETE',
-        'https://api.venafi.cloud/outagedetection/v1/applications/' + appGUID,
+        'PUT',
+        'https://api.venafi.cloud/outagedetection/v1/applications/' + app_guid,
         headers={
             'accept': '*/*',
             'tppl-api-key': api_key
