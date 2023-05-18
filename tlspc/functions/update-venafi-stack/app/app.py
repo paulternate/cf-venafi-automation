b3dc4ffbe8f88098d4735d2d05e5fb61b7990c4c
Alan McGinlay <amcginlay@gmail.com>
2023-05-18 14:01:03 +0100

    generalized building update params

--- tlspc/functions/update-venafi-stack/app/app.py
+++ tlspc/functions/update-venafi-stack/app/app.py
@@ -3,24 +3,39 @@ import datetime
 import traceback
 import boto3
 
+cloudformation = boto3.client('cloudformation')
+
+def get_stack_parameter_keys(stack_id):
+    response = cloudformation.describe_stacks(StackName=stack_id)
+    parameters = response['Stacks'][0]['Parameters']
+    parameter_keys = [item['ParameterKey'] for item in parameters]
+    return parameter_keys
+
+def build_update_parameters(stack_id):
+    timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+    parameter_keys = get_stack_parameter_keys(stack_id)
+    update_parameters = []
+    for parameter_key in parameter_keys:
+        if parameter_key == 'UpdateTrigger':
+            update_parameters.append({
+                    'ParameterKey': 'UpdateTrigger',
+                    'ParameterValue': timestamp
+                })
+        else:
+            update_parameters.append({
+                    'ParameterKey': parameter_key,
+                    'UsePreviousValue': True
+                })
+    return update_parameters
+
 def lambda_handler(event, context):
     stack_id = os.getenv("TARGET_STACK_ID")
-    cloudformation = boto3.client('cloudformation')
-    timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
     try:
+        update_parameters = build_update_parameters(stack_id)
         response = cloudformation.update_stack(
             StackName=stack_id,
             UsePreviousTemplate=True,
-            Parameters=[
-                {
-                    'ParameterKey': 'TLSPCAPIKey',
-                    'UsePreviousValue': True
-                },
-                {
-                    'ParameterKey': 'UpdateTrigger',
-                    'ParameterValue': timestamp
-                }
-            ]
+            Parameters=update_parameters
         )
         response = None
     except Exception as e:
