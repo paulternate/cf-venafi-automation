f14c10d264ed7884ea9a3fd66278be0e680f039f
Alan McGinlay <amcginlay@gmail.com>
2023-05-22 17:42:03 +0100

    added stack-marker logic

--- tlspc/functions/venafi-stack-updater/app/app.py
+++ tlspc/functions/venafi-stack-updater/app/app.py
@@ -2,8 +2,10 @@ import os
 import datetime
 import traceback
 import boto3
+from botocore.exceptions import ClientError
 
 cloudformation = boto3.client('cloudformation')
+s3 = boto3.client('s3')
 
 def get_stack_parameter_keys(stack_id):
     response = cloudformation.describe_stacks(StackName=stack_id)
@@ -28,16 +30,41 @@ def build_update_parameters(stack_id):
                 })
     return update_parameters
 
-def lambda_handler(event, context):
-    stack_id = os.getenv("TARGET_STACK_ID")
+def stack_marker_present(target_s3_bucket, stack_id):
     try:
-        update_parameters = build_update_parameters(stack_id)
-        response = cloudformation.update_stack(
-            StackName=stack_id,
-            UsePreviousTemplate=True,
-            Parameters=update_parameters
-        )
-        response = None
+        object_prefix = 'stacks/'
+        s3.head_object(Bucket=target_s3_bucket, Key=f'{object_prefix}{stack_id}.txt')
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return False
+        else:
+            raise
+    return True
+
+def write_stack_marker(target_s3_bucket, stack_id):
+    object_prefix = f'stacks/'
+    s3.put_object(Bucket=target_s3_bucket, Key=f'{object_prefix}{stack_id}.txt', Body='for system usage, do not delete')
+
+# the stack marker logic is required because the AWS::Scheduler::Schedule fires its first event right at the point of creation
+# this means the Create event is followed IMMEDIATELY by the first Update event and there's nothing developers can do to alter this.
+# this is NOT the required behavior for certificate renewals so this logic, used whenever the AWS::Scheduler::Schedule resource fires, checks for the existence of an S3 object which represents the Stack (stack_marker_present())
+# when this returns False, it knows this is the very first invocation for this Stack so it lays down a marker (write_stack_marker()) and skips the Update.
+# Every subsequent invocation sees the marker, returns True, and commits to the cloudformation.update_stack() as normal.
+
+def lambda_handler(event, context):
+    target_s3_bucket = os.getenv("TARGET_S3_BUCKET")
+    stack_id = os.getenv("STACK_ID")
+    try:
+        if not stack_marker_present(target_s3_bucket, stack_id):
+            write_stack_marker(target_s3_bucket, stack_id)
+        else:
+            update_parameters = build_update_parameters(stack_id)
+            response = cloudformation.update_stack(
+                StackName=stack_id,
+                UsePreviousTemplate=True,
+                Parameters=update_parameters
+            )
+            response = None
     except Exception as e:
         response = traceback.format_exc()
     finally:
