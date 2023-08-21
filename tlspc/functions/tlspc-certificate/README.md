99c1c8220007e401f20603ee66e537e94ac05aeb
Alan McGinlay <amcginlay@gmail.com>
2023-08-21 22:16:31 +0100

    ACM logic, first attempt

--- tlspc/functions/tlspc-certificate/README.md
+++ tlspc/functions/tlspc-certificate/README.md
@@ -1,1 +1,8 @@
-# Put documentation here
+# NOTES
+
+With the policy (app\cit) in place, the certificate template/function will have the same effect as the following `vcert enroll` command
+
+For example:
+```
+vcert enroll -k deaddead-dead-dead-dead-deaddeaddead -z "team999-app\team999-cit" --cn team999.venafidemo.com
+```
--- tlspc/functions/tlspc-certificate/app/app.py
+++ tlspc/functions/tlspc-certificate/app/app.py
@@ -1,6 +1,5 @@
 import logging
 import time
-from datetime import datetime
 import json
 import urllib3
 import traceback
@@ -9,6 +8,8 @@ from vcert import (CertificateRequest, venafi_connection, CSR_ORIGIN_SERVICE)
 import boto3
 import botocore
 from botocore.exceptions import ClientError
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -31,7 +32,8 @@ def get_parameters(event):
     target_s3_bucket = str(event['ResourceProperties']['TargetS3Bucket'])
     if not target_s3_bucket:
         target_s3_bucket = 'venafi-tlspc-certificates-' + get_aws_account()
-    return api_key, common_name, zone, validity_hours, private_key_passphrase, target_s3_bucket
+    publish_to_acm = str(event['ResourceProperties']['PublishToACM'])
+    return api_key, common_name, zone, validity_hours, private_key_passphrase, target_s3_bucket, publish_to_acm
 
 def redact_sensitive_info(json_data, sensitive_key, redacted_value='***'):
     data = json.loads(json_data)
@@ -66,7 +68,7 @@ def get_stack_output_value(event, output_key):
     except:
         return None
 
-def retreive_cert_with_retry(conn, request):
+def retrieve_cert_with_retry(conn, request):
     timeout = 720 # 12 mins (Lambda itself has a hard limit at 15 mins)
     interval = 15
     start_time = time.time()
@@ -75,12 +77,12 @@ def retreive_cert_with_retry(conn, request):
     while True:
         try:
             attempt += 1
-            logger.info(f'retreive_cert_with_retry() - attempt {attempt}')
+            logger.info(f'retrieve_cert_with_retry() - attempt {attempt}')
             cert = conn.retrieve_cert(request)
             if cert is not None:
                 break
         except Exception as e:
-            logger.info(f'retreive_cert_with_retry() - attempt {attempt} failed with exception: {str(e)}')
+            logger.info(f'retrieve_cert_with_retry() - attempt {attempt} failed with exception: {str(e)}')
 
         elapsed_time = time.time() - start_time
         if elapsed_time >= timeout:
@@ -89,7 +91,7 @@ def retreive_cert_with_retry(conn, request):
         time.sleep(interval)
         
     if cert is None:
-        raise Exception('retreive_cert_with_retry() - failed to retrieve cert within {timeout} seconds.')
+        raise Exception('retrieve_cert_with_retry() - failed to retrieve cert within {timeout} seconds.')
     
     return cert
 
@@ -119,16 +121,17 @@ def get_cert_expiry(api_key, cert_id):
 def store_cert_in_s3(target_s3_bucket, physical_resource_id, common_name, cert, stack_id, cert_request_id):
     object_prefix = f'certs/{common_name}/{physical_resource_id}/'
     s3 = boto3.client('s3')
-    s3.put_object(Bucket=target_s3_bucket, Key=f'{object_prefix}{common_name}.cert', Body=cert.full_chain)
-    s3.put_object(Bucket=target_s3_bucket, Key=f'{object_prefix}{common_name}.key', Body=cert.key)
-    logger.info(f'cert objects saved to s3: target_s3_bucket={target_s3_bucket} object_prefix={object_prefix} common_name={common_name}')
     tags = [
         {'Key': 'StackId', 'Value': stack_id},
         {'Key': 'CertRequestId', 'Value': cert_request_id}
     ]
+    s3.put_object(Bucket=target_s3_bucket, Key=f'{object_prefix}{common_name}.chain', Body=cert.full_chain)
+    s3.put_object_tagging(Bucket=target_s3_bucket, Key=f'{object_prefix}{common_name}.chain', Tagging={'TagSet': tags})
+    s3.put_object(Bucket=target_s3_bucket, Key=f'{object_prefix}{common_name}.cert', Body=cert.cert)
     s3.put_object_tagging(Bucket=target_s3_bucket, Key=f'{object_prefix}{common_name}.cert', Tagging={'TagSet': tags})
+    s3.put_object(Bucket=target_s3_bucket, Key=f'{object_prefix}{common_name}.key', Body=cert.key)
     s3.put_object_tagging(Bucket=target_s3_bucket, Key=f'{object_prefix}{common_name}.key', Tagging={'TagSet': tags})
-    logger.info(f'cert object tagged: StackId={stack_id} CertRequestId={cert_request_id}')
+    logger.info(f'successfully stored and tagged key material objects in s3: target_s3_bucket={target_s3_bucket} object_prefix={object_prefix} common_name={common_name}')
     aws_region = get_aws_region()
     return f'https://s3.console.aws.amazon.com/s3/buckets/{target_s3_bucket}?region={aws_region}&prefix={object_prefix}'
 
@@ -174,26 +177,73 @@ def get_latest_cert_request_id(target_s3_bucket, event):
         logger.info(f'previous cert request ids do not match (continuing): value_from_outputs={value_from_outputs} value_from_s3={value_from_s3}')
     return value_from_s3 if value_from_s3 is not None else value_from_outputs
 
+def decrypt_private_key(encrypted_key_content, private_key_passphrase):
+    key = serialization.load_pem_private_key(
+        encrypted_key_content.encode('utf-8'),
+        password=private_key_passphrase.encode('utf-8'),
+        backend=default_backend()
+    )
+    key_pem = key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    key_text = key_pem.decode('utf-8')
+    return key_text
+
+def import_cert_into_acm(cert, private_key_passphrase, reimport_arn=None):
+    logger.info(f'importing key material into ACM')
+    acm = boto3.client('acm')
+    
+    response = None
+    if reimport_arn is None:
+        # IMPORT (CertificateArn omitted)
+        response = acm.import_certificate(
+            CertificateChain=cert.full_chain,
+            Certificate=cert.cert,
+            PrivateKey=decrypt_private_key(cert.key, private_key_passphrase)
+        )
+    else:
+        # RE-IMPORT
+        response = acm.import_certificate(
+            CertificateChain=cert.full_chain,
+            Certificate=cert.cert,
+            PrivateKey=decrypt_private_key(cert.key, private_key_passphrase),
+            CertificateArn=reimport_arn
+        )
+    arn = response['CertificateArn']
+    logger.info(f'successfully imported key material into ACM: ARN={arn}')
+    aws_region = get_aws_region()
+    cert_uid = arn[arn.rfind('/') + 1:]
+    return f'https://{aws_region}.console.aws.amazon.com/acm/home?#/certificates/{cert_uid}'
+
 def create_handler(event, context):
     responseData = {}
     logger.info('RequestType: Create')
     ###########
     # code here
     ###########
-    api_key, common_name, zone, validity_hours, private_key_passphrase, target_s3_bucket = get_parameters(event)
+    api_key, common_name, zone, validity_hours, private_key_passphrase, target_s3_bucket, publish_to_acm = get_parameters(event)
     conn = venafi_connection(api_key=api_key)
     request = CertificateRequest(common_name=common_name)
     request.validity_hours = validity_hours
     request.csr_origin = CSR_ORIGIN_SERVICE
     request.key_password = private_key_passphrase
+    # ---------------------------------------------
+    # TODO Digicert sometimes fails with the following error so perhaps we need some retry logic here like we do in retrieve_cert_with_retry
+    # File \"/var/task/vcert/connection_cloud.py\", line 406, in request_cert\n    request.cert_guid = data['certificateRequests'][0]['certificateIds'][0]\nIndexError: list index out of range\n"}}
     conn.request_cert(request, zone)
+    # ---------------------------------------------
     set_latest_cert_request_id_s3(target_s3_bucket, event, request.id) # important to record this in case next steps fail/timeout
-    cert = retreive_cert_with_retry(conn, request)
+    cert = retrieve_cert_with_retry(conn, request)
     cert_expiry = get_cert_expiry(api_key, request.cert_guid)
     logger.info(f'certificate retrieved: request.id={request.id} request.cert_guid={request.cert_guid} cert_expiry={cert_expiry}')
 
-    s3_url= store_cert_in_s3(target_s3_bucket, request.id, common_name, cert, event['StackId'], request.id)
-    logger.info('objects stored')
+    s3_url = store_cert_in_s3(target_s3_bucket, request.id, common_name, cert, event['StackId'], request.id)    
+    acm_url, acm_arn = None, None
+    if publish_to_acm != 'false':
+        acm_url, acm_arn  = import_cert_into_acm(cert, private_key_passphrase)
+    
     ###########
     responseData['PhysicalResourceId'] = request.id
     responseData['LatestCertRequestId'] = request.id
@@ -201,6 +251,8 @@ def create_handler(event, context):
     responseData['LatestCertExpiry'] = cert_expiry
     responseData['TargetS3Bucket'] = target_s3_bucket
     responseData['S3URL'] = s3_url
+    responseData['ACMURL'] = acm_url
+    responseData['ACMARN'] = acm_arn
     return responseData
 
 def update_handler(event, context):
@@ -210,7 +262,7 @@ def update_handler(event, context):
     ###########
     # code here
     ###########
-    api_key, common_name, _, _, _, target_s3_bucket = get_parameters(event)
+    api_key, common_name, _, _, private_key_passphrase, target_s3_bucket, publish_to_acm = get_parameters(event)
     latest_cert_request_id = get_latest_cert_request_id(target_s3_bucket, event)
     conn = venafi_connection(api_key=api_key)
 
@@ -221,20 +273,26 @@ def update_handler(event, context):
     logger.info(f'certificate renewed: request.id={request.id} cert_id={cert_id}')
 
     # after conn.renew_cert, request.cert_guid is only set after a successful call to conn.retrieve_cert() ... unless you're in AWS! (see get_cert_id())
-    cert = retreive_cert_with_retry(conn, request)
+    cert = retrieve_cert_with_retry(conn, request)
     cert_id = get_cert_id(api_key, request.id)
     cert_expiry = get_cert_expiry(api_key, cert_id)
     logger.info(f'renewed certificate retrieved: request.id={request.id} cert_id={cert_id} cert_expiry={cert_expiry}')
 
     s3_url = store_cert_in_s3(target_s3_bucket, physical_resource_id, common_name, cert, event['StackId'], request.id) # physical_resource_id used to ensure consistency with first CR (version history)
-    logger.info('objects stored')
+    acm_url, acm_arn = None, None
+    if publish_to_acm != 'false':
+        reimport_arn = get_stack_output_value(event, 'ACMARN')
+        acm_url, acm_arn = import_cert_into_acm(cert, private_key_passphrase, reimport_arn)
+    
     ###########
-    responseData['PhysicalResourceId'] = physical_resource_id # fix PhysicalResourceId to first CR, to CFN happy
+    responseData['PhysicalResourceId'] = physical_resource_id # fix PhysicalResourceId to first CR, to keep CFN happy
     responseData['LatestCertRequestId'] = request.id
     responseData['LatestCertId'] = cert_id # should be able to use request.cert_guid, but ¯\_(ツ)_/¯
     responseData['LatestCertExpiry'] = cert_expiry
     responseData['TargetS3Bucket'] = target_s3_bucket
     responseData['S3URL'] = s3_url
+    responseData['ACMURL'] = acm_url
+    responseData['ACMARN'] = acm_arn
     return responseData
 
 def delete_handler(event, context):
@@ -244,7 +302,7 @@ def delete_handler(event, context):
     ###########
     # code here
     ###########
-    api_key, _, _, _, _, _ = get_parameters(event)
+    api_key, _, _, _, _, _, _ = get_parameters(event)
     latest_cert_id = get_stack_output_value(event, 'LatestCertId')
     logger.info('latest_cert_id: ' + latest_cert_id)
     payload = {
